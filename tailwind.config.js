3f11deaf7ffd13badce9dd460f000ec30f0fe25f
Nazmulhudashawon <shawonnazmulhuda@gmail.com>
2023-02-08 18:48:00 +0600

    3rd  commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,30 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     "./app/**/*.{js,ts,jsx,tsx}",
+//     "./pages/**/*.{js,ts,jsx,tsx}",
+//     "./components/**/*.{js,ts,jsx,tsx}",
+
+//   ],
+//   theme: {
+//     extend: {
+//       fontfamily: {
+//         Poppins:['poppins'],
+//       }
+//     },
+//   },
+//   plugins: [],
+// }
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
+  purge: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {
-      fontfamily: {
-        Poppins:['poppins'],
-      }
-    },
+  subject: {
+    extend: {},
   },
-  plugins: [],
-}
+  plugins: [require("@tailwindcss/typography"),require('@tailwindcss/forms'),],
+  
+};
